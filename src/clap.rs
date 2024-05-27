d93c9ace08af47d4ae6a652ba045046812e580b2
Grace Murphy <localghost385@gmail.com>
2024-05-27 14:01:28 +0100

    v0.1.0 preparing for release

--- src/clap.rs
+++ src/clap.rs
@@ -1,18 +1,50 @@
 use clap::*;
 
-//----------[ Args ]----------//
-pub fn clap_parse() -> ArgMatches {
-	let cmd = clap::Command::new("cargo")
-		.bin_name("tetrs")
-		.about("A tui Tetris game written in Rust!")
-		//----------[ Version ]----------//
-		.arg(
-			Arg::new("version")
-				.long("version")
-				.short('v')
-				.action(ArgAction::SetTrue),
-		);
-		//-------------------------------//
-	cmd.get_matches()
+use crate::fonts::define_fonts;
+
+fn get_fonts() -> Vec<&'static str> {
+    let fonts = define_fonts();
+
+    fonts.keys().cloned().collect()
 }
-//----------------------------//
+
+pub fn clap_parse() -> ArgMatches {
+    let cmd = clap::Command::new("cargo")
+        .bin_name("glyphrs")
+		.before_help("-----------------------------\n▄▀  █   ▀▄▀ █▀▄ █▄█   █▀▄ ▄▀▀\n▀▄█ █▄▄  █  █▀  █ █ ▄ █▀▄ ▄██ \n-----------------------------")
+        .about("A text art generator written in Rust")
+		.arg(
+			Arg::new("input")
+            .required_unless_present("version")
+            .help("the string to be converted"),
+		)
+        .arg(
+            Arg::new("font")
+            .long("font")
+            .short('f')
+            .default_value("blocks_in_two_lines")
+            .value_parser(get_fonts())
+            .help("set the font"),
+
+        )
+        .arg(
+            Arg::new("version")
+            .long("version")
+            .short('v')
+            .action(ArgAction::SetTrue)
+            .help("Print version information"),
+        );
+
+    cmd.get_matches()
+}
+
+pub fn handle_args() -> ArgMatches {
+    let matches: ArgMatches = clap_parse();
+    let version: bool = *matches.get_one("version").unwrap();
+    if version {
+        println!("glyphrs v{}", env!("CARGO_PKG_VERSION"));
+        std::process::exit(0);
+    }
+
+    matches
+}
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,56 @@
-use glyphrs::clap;
+use glyphrs::{clap, fonts::define_fonts};
 
-fn map_search() {
-    
+fn sanitize_input(input: String) -> String {
+    let mut output = input.clone();
+    for characters in input.chars() {
+        if characters.is_alphabetic() {
+            output = output.to_lowercase();
+        }
+    }
+    output
+}
+
+fn map_search(key: char, font: &str) -> Vec<&'static str> {
+    let fonts = define_fonts();
+
+    let map = fonts.get(font).unwrap().clone();
+
+    let default: Vec<&str> = vec!["", ""];
+    let value = map.get(&key).unwrap_or(&default).to_vec().clone();
+
+    value
+}
+
+fn string_composite(characters: Vec<Vec<&str>>) -> Vec<String> {
+    let mut output: Vec<String> = vec![];
+
+    for _ in 0..characters[0].len() {
+        output.push(String::new());
+    }
+
+    for character in characters {
+        for i in 0..character.len() {
+            output[i] += character[i];
+            output[i] += " ";
+        }
+    }
+
+    output
 }
 
 fn main() {
-    println!("Hello, world!");
+    let matches = clap::handle_args();
+    let input: String = matches.get_one::<String>("input").unwrap().to_string();
+    let font: String = matches.get_one::<String>("font").unwrap().to_string();
+
+    let input = sanitize_input(input);
+
+    let mut map_values: Vec<Vec<&str>> = vec![];
+    for c in input.chars() {
+        map_values.push(map_search(c, &font));
+    }
+    let output: Vec<String> = string_composite(map_values);
+
+    println!("{}", output[0]);
+    println!("{}", output[1]);
 }
